3062c4ff101cd121a4726a0a695a0a829fc4111a
targeral <targeral@outlook.com>
2024-04-10 14:56:53 +0800

    feat: add get github token guide

--- apps/browser-extension/options/Storage/Github.tsx
+++ apps/browser-extension/options/Storage/Github.tsx
@@ -3,11 +3,12 @@ import {
   Box,
   Card,
   Link,
+  Button,
+  Modal,
   Switch,
   TextField,
   Tooltip,
-  Typography,
-  // Button
+  Typography
 } from "@mui/material"
 import IconButton from "@mui/material/IconButton"
 import React, { useEffect, useState } from "react"
@@ -34,28 +35,42 @@ export const GithubStorageSetting = ({
   onEnableChange
 }: GithubStorageSettingProps) => {
   const [showToken, setShowToken] = useState<boolean>(false)
-  const [token, _setToken, { setRenderValue: setToken, setStoreValue: saveToken }] = useStorage<string>(
+  const [openModal, setOpenModal] = useState<boolean>(false)
+  const [
+    token,
+    _setToken,
+    { setRenderValue: setToken, setStoreValue: saveToken }
+  ] = useStorage<string>(
     {
       key: GithubStorageKey.TOKEN,
       instance
     },
     ""
   )
-  const [repo, _setRepo, { setRenderValue: setRepo, setStoreValue: saveRepo }] = useStorage<string>(
-    {
-      key: GithubStorageKey.REPO,
-      instance
-    },
-    ""
-  )
-  const [owner, _setOwner, { setRenderValue: setOwner, setStoreValue: saveOwner }] = useStorage<string>(
+  const [repo, _setRepo, { setRenderValue: setRepo, setStoreValue: saveRepo }] =
+    useStorage<string>(
+      {
+        key: GithubStorageKey.REPO,
+        instance
+      },
+      ""
+    )
+  const [
+    owner,
+    _setOwner,
+    { setRenderValue: setOwner, setStoreValue: saveOwner }
+  ] = useStorage<string>(
     {
       key: GithubStorageKey.OWNER,
       instance
     },
     ""
   )
-  const [email, _setEmail, { setRenderValue: setEmail, setStoreValue: saveEmail }] = useStorage<string>(
+  const [
+    email,
+    _setEmail,
+    { setRenderValue: setEmail, setStoreValue: saveEmail }
+  ] = useStorage<string>(
     {
       key: GithubStorageKey.EMAIL,
       instance
@@ -95,123 +110,167 @@ export const GithubStorageSetting = ({
   }
 
   const save = () => {
-    saveOwner(owner);
-    saveEmail(email);
-    saveRepo(repo);
-    saveToken(token);
+    saveOwner(owner)
+    saveEmail(email)
+    saveRepo(repo)
+    saveToken(token)
   }
 
+  const handleModalOpen = () => setOpenModal(true)
+  const handleModalClose = () => setOpenModal(false)
+
   return (
-    <Accordion sx={{ width: "100%" }}>
-      <AccordionSummary aria-controls="github-content" id="github-header">
-        <Box
-          sx={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center"
-          }}>
-          <Typography>Github</Typography>
-          <Switch onChange={handleSwitch} checked={enable} />
-        </Box>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            gap: 1
-          }}>
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <TextField
-              label="Github Repo:"
-              value={repo}
-              onChange={handleRepoChange}
-              onBlur={save}
-              sx={{ flex: "1 1 auto" }}
-            />
-            <Tooltip
-              title={
-                <>
-                  <Typography variant="body1">
-                    A Github repository for storing data.{" "}
-                    <Link
-                      href="https://github.com/new"
-                      target="_blank"
-                      color="#ffc107"
-                      rel="noreferrer">
-                      To Create
-                    </Link>
-                  </Typography>
-                </>
-              }>
-              <IconButton>
-                <HelpOutlineIcon />
-              </IconButton>
-            </Tooltip>
+    <>
+      <Accordion sx={{ width: "100%" }}>
+        <AccordionSummary aria-controls="github-content" id="github-header">
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center"
+            }}>
+            <Typography>Github</Typography>
+            <Switch onChange={handleSwitch} checked={enable} />
           </Box>
-          <TextField
-            label="Github owner:"
-            value={owner}
-            onChange={handleOwnerChange}
-            onBlur={save}
-          />
-          <TextField
-            label="Owner email:"
-            value={email}
-            onChange={handleEmailChange}
-            onBlur={save}
-          />
-          <Box sx={{ display: "flex", alignItems: "center" }}>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              gap: 1
+            }}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <TextField
+                label="Github Repo:"
+                value={repo}
+                onChange={handleRepoChange}
+                onBlur={save}
+                sx={{ flex: "1 1 auto" }}
+              />
+              <Tooltip
+                title={
+                  <>
+                    <Typography variant="body1">
+                      A Github repository for storing data.{" "}
+                      <Link
+                        href="https://github.com/new"
+                        target="_blank"
+                        color="#ffc107"
+                        rel="noreferrer">
+                        To Create
+                      </Link>
+                    </Typography>
+                  </>
+                }>
+                <IconButton>
+                  <HelpOutlineIcon />
+                </IconButton>
+              </Tooltip>
+            </Box>
             <TextField
-              sx={{ flex: "1 1 auto" }}
-              type={showToken ? "text" : "password"}
-              label="Github Token:"
-              value={token}
-              onChange={handleTokenChange}
+              label="Github owner:"
+              value={owner}
+              onChange={handleOwnerChange}
               onBlur={save}
             />
-            <Tooltip
-              title={
-                <div>
-                  <Typography variant="h6">How to get Github Token</Typography>
-                  <Typography variant="body1">
-                    1. Click on this{" "}
-                    <Link
-                      href="https://github.com/settings/personal-access-tokens/new"
-                      target="_blank"
-                      color="#ffc107"
-                      rel="noreferrer">
-                      link
-                    </Link>{" "}
-                    to create
-                  </Typography>
-                  <Typography variant="body1">
-                    2. Find on the page:
-                    <Typography sx={{ marginLeft: "20px" }} variant="body2">
-                      {`=>\n`} "Repository access"
+            <TextField
+              label="Owner email:"
+              value={email}
+              onChange={handleEmailChange}
+              onBlur={save}
+            />
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <TextField
+                sx={{ flex: "1 1 auto" }}
+                type={showToken ? "text" : "password"}
+                label="Github Token:"
+                value={token}
+                onChange={handleTokenChange}
+                onBlur={save}
+              />
+              <Button
+                size="small"
+                onClick={handleModalOpen}
+                variant="text">
+                如何获取?
+              </Button>
+              {/* <Tooltip
+                title={
+                  <div>
+                    <Typography variant="h6">
+                      How to get Github Token
                     </Typography>
-                    <Typography sx={{ marginLeft: "20px" }} variant="body2">
-                      {`=>\n`} "Only select repositories"
+                    <Typography variant="body1">
+                      1. Click on this{" "}
+                      <Link
+                        href="https://github.com/settings/personal-access-tokens/new"
+                        target="_blank"
+                        color="#ffc107"
+                        rel="noreferrer">
+                        link
+                      </Link>{" "}
+                      to create
                     </Typography>
-                    <Typography sx={{ marginLeft: "20px" }} variant="body2">
-                      {" "}
-                      {`=>\n`}"Select Your Repo"
+                    <Typography variant="body1">
+                      2. Find on the page:
+                      <Typography sx={{ marginLeft: "20px" }} variant="body2">
+                        {`=>\n`} "Repository access"
+                      </Typography>
+                      <Typography sx={{ marginLeft: "20px" }} variant="body2">
+                        {`=>\n`} "Only select repositories"
+                      </Typography>
+                      <Typography sx={{ marginLeft: "20px" }} variant="body2">
+                        {" "}
+                        {`=>\n`}"Select Your Repo"
+                      </Typography>
                     </Typography>
-                  </Typography>
-                </div>
-              }>
-              <IconButton>
-                <HelpOutlineIcon />
-              </IconButton>
-            </Tooltip>
+                  </div>
+                }>
+                <IconButton>
+                  <HelpOutlineIcon />
+                </IconButton>
+              </Tooltip> */}
+            </Box>
           </Box>
-        </Box>
-        {/* <Box sx={{ marginTop: '10px', display: 'flex', justifyContent: 'flex-end' }}>
+          {/* <Box sx={{ marginTop: '10px', display: 'flex', justifyContent: 'flex-end' }}>
           <Button onClick={save} variant="contained">保存</Button>
         </Box> */}
-      </AccordionDetails>
-    </Accordion>
+        </AccordionDetails>
+      </Accordion>
+      <Modal
+        open={openModal}
+        onClose={handleModalClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 600,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4
+          }}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            如何生成 Github Token?
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <ol>
+              <li>首先点击该 <Link href="https://github.com/settings/personal-access-tokens/new">链接</Link>进入创建 Token 页面。</li>
+              <li>输入必须输入的 <b>Token name</b>。</li>
+              <li>对于 <b>Repository access</b> 选择 <b>Only select repositories</b>，并选择你要存储书签数据的仓库。</li>
+              <li>在 <b>Repository permissions</b> 中需要将 <b>Repository permissions</b> 的 <i>Codespaces</i>、<i>Commit statuses</i>、<i>Contents</i> 的 Access（权限）从 No access 改为 Read and Write。</li>
+              <li>最后点击页面底部的 Generate Token 按钮，将会获取 token。</li>
+            </ol>
+          </Typography>
+        </Box>
+      </Modal>
+    </>
   )
 }
